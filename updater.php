def97bf9e4ef310d1a17f9cd2026f7f06f507046
Alain DRILLON <perso@drillon-ala.in>
2017-01-07 17:22:00 +0100

    Made updater wait during a certain period of time before updating again

--- updater.php
+++ updater.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 if (! is_file('config.ini')) {
@@ -11,26 +12,35 @@ if (! is_dir('users')) {
     die('Nothing to update.');
 }
 
-$inifiles = scandir('users');
-foreach ($inifiles as $ini) {
-    if (pathinfo('users/' . $ini, PATHINFO_EXTENSION) != 'ini') {
-        continue;
+$sleeptime = 60;
+if (array_key_exists('sleep_time', $config)) {
+    $sleeptime = $config['sleep_time'];
+}
+
+while (true) {
+    $inifiles = scandir('users');
+    foreach ($inifiles as $ini) {
+        if (pathinfo('users/' . $ini, PATHINFO_EXTENSION) != 'ini') {
+            continue;
+        }
+
+        $username = pathinfo('users/' . $ini, PATHINFO_FILENAME);
+
+        $calendars = parse_ini_file('users/' . $ini, true);
+
+        foreach ($calendars as $cal) {
+            $props = array(
+                "tag" => "VCALENDAR",
+                "ICAL:calendar-color" => $cal['color'],
+                "D:displayname" => $cal['display_name'],
+            );
+
+            $caldata = file_get_contents($cal['url']);
+
+            file_put_contents($davpath . '/' . $username . '/' . $cal['name'] . '.props', json_encode($props));
+            file_put_contents($davpath . '/' . $username. '/' . $cal['name'], $caldata);
+        }
     }
 
-    $username = pathinfo('users/' . $ini, PATHINFO_FILENAME);
-
-    $calendars = parse_ini_file('users/' . $ini, true);
-
-    foreach ($calendars as $cal) {
-        $props = array(
-            "tag" => "VCALENDAR",
-            "ICAL:calendar-color" => $cal['color'],
-            "D:displayname" => $cal['display_name'],
-        );
-
-        $caldata = file_get_contents($cal['url']);
-
-        file_put_contents($davpath . '/' . $username . '/' . $cal['name'] . '.props', json_encode($props));
-        file_put_contents($davpath . '/' . $username. '/' . $cal['name'], $caldata);
-    }
+    sleep($sleeptime);
 }
